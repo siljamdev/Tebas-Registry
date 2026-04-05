db2f3af8275fb4d74b96a31b81fe615d262bc939
Dumbelfo <dumbelfo08@gmail.com>
2026-04-05 13:51:31 +0200

    Added actions

---
+++ generate_readme.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+readme="README.md"
+
+cat > "$readme" << 'EOF'
+# Tebas Registry
+Official template & plugin registry for [Tebas](https://github.com/siljamdev/Tebas)
+
+## Templates
+EOF
+
+for dir in templates/*/; do
+    folder=$(basename "$dir")
+    name=$(cat "$dir/name.txt" 2>/dev/null || echo "$folder")
+    desc=$(cat "$dir/description.txt" 2>/dev/null)
+    if [ -n "$desc" ]; then
+        echo "- [$name](./templates/$folder): $desc" >> "$readme"
+    else
+        echo "- [$name](./templates/$folder)" >> "$readme"
+    fi
+done
+
+echo "" >> "$readme"
+echo "## Plugins" >> "$readme"
+
+for dir in plugins/*/; do
+    folder=$(basename "$dir")
+    name=$(cat "$dir/name.txt" 2>/dev/null || echo "$folder")
+    desc=$(cat "$dir/description.txt" 2>/dev/null)
+    if [ -n "$desc" ]; then
+        echo "- [$name](./plugins/$folder): $desc" >> "$readme"
+    else
+        echo "- [$name](./plugins/$folder)" >> "$readme"
+    fi
+done
