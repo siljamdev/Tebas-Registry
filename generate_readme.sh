6240561ebbe975631c0045d94a07a07d77774914
Dumbelfo <dumbelfo08@gmail.com>
2026-05-17 16:35:10 +0200

    Updated stats and unirun

--- generate_readme.sh
+++ generate_readme.sh
@@ -10,7 +10,8 @@ cat > "$readme" << EOF
 # Tebas Registry
 Official template & plugin registry for [Tebas](https://github.com/siljamdev/Tebas)  
 Built for Tebas $version  
-Download files from the [Releases](https://github.com/siljamdev/Tebas-Registry/releases/latest)
+Use the link \`https://github.com/siljamdev/Tebas-Registry\` for Tebas option \`registry.url\`  
+Download files manually from the [Releases](https://github.com/siljamdev/Tebas-Registry/releases/latest)
 
 ## Contributing
 Before making a PR, run \`build.sh\` with the latest tebas version installed and make sure all templates and plugins succesfully build  
